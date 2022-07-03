bb72b7e24ec1ebff2c349dfa2b1ec63f588e98f6
Himanshu Shekhar2 <himanshu.shekhar2@bajajfinserv.in>
2022-07-03 21:58:28 +0530

    UX changes

--- src/App.js
+++ src/App.js
@@ -1,133 +1,41 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./index.css";
+import { data } from "./data";
 import { Tree } from "antd";
-import { useState } from "react";
-const data = [
-  {
-    title: "Sports",
-    key: null,
-  },
-  {
-    title: "Soccer",
-    key: null,
-  },
-  {
-    title: "Rugby",
-    key: null,
-  },
-  {
-    title: "IPL",
-    key: "Sports",
-  },
-  {
-    title: "EPL",
-    key: "Sports",
-  },
-  {
-    title: "NBA",
-    key: "Sports",
-  },
-  {
-    title: "Mumbai Indians",
-    key: "IPL",
-  },
-  {
-    title: "gyug",
-    key: "Mumbai Indians",
-  },
-  {
-    title: "gyug",
-    key: "Rajasthan Royals",
-  },
-  {
-    title: "gyug",
-    key: "Gujarat Titans",
-  },
-  {
-    title: "Rajasthan Royals",
-    key: "IPL",
-  },
-  {
-    title: "Gujarat Titans",
-    key: "IPL",
-  },
-  {
-    title: "Arsenal",
-    key: "EPL",
-  },
-  {
-    title: "Chelsea",
-    key: "EPL",
-  },
-  {
-    title: "Manchester United",
-    key: "EPL",
-  },
-];
-const App = () => {
-  const [expandedKeys, setExpandedKeys] = useState(["0-0-0", "0-0-1"]);
-  const [checkedKeys, setCheckedKeys] = useState(["0-0-0"]);
-  const [selectedKeys, setSelectedKeys] = useState([]);
-  const [autoExpandParent, setAutoExpandParent] = useState(true);
 
-  function createTree(list) {
-    var map = {},
-      node,
-      roots = [],
-      i;
+function createTree(list) {
+  var map = {},
+    node,
+    roots = [],
+    i;
 
-    for (i = 0; i < list.length; i += 1) {
-      map[list[i].title] = i; // initialize the map
-      list[i].children = []; // initialize the children
-    }
-
-    for (i = 0; i < list.length; i += 1) {
-      node = list[i];
-      if (node.key !== null) {
-        // if you have dangling branches check that map[node.key] exists
-        list[map[node.key]].children.push(node);
-      } else {
-        roots.push(node);
-      }
-    }
-    return roots;
+  for (i = 0; i < list.length; i += 1) {
+    map[list[i].title] = i; // initialize the map
+    list[i].children = []; // initialize the children
   }
-  debugger;
-  const treeData = createTree(data);
 
-  const onExpand = (expandedKeysValue) => {
-    console.log("onExpand", expandedKeysValue); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
-    // or, you can remove all expanded children keys.
+  for (i = 0; i < list.length; i += 1) {
+    node = list[i];
+    if (node.parentId !== null) {
+      // if you have dangling branches check that map[node.key] exists
+      list[map[node.parentId]].children.push(node);
+    } else {
+      roots.push(node);
+    }
+  }
 
-    setExpandedKeys(expandedKeysValue);
-    setAutoExpandParent(false);
-  };
+  return roots;
+}
 
-  const onCheck = (checkedKeysValue) => {
-    console.log("onCheck", checkedKeysValue);
-    setCheckedKeys(checkedKeysValue);
-  };
-
-  const onSelect = (selectedKeysValue, info) => {
-    console.log("onSelect", info);
-    setSelectedKeys(selectedKeysValue);
-  };
+const App = () => {
+  const treeData = createTree(
+    data.map((item) => ({ ...item, key: item.title }))
+  );
 
   return (
     <div className="treeWrapper">
-      <Tree
-        showLine
-        checkable
-        onExpand={onExpand}
-        expandedKeys={expandedKeys}
-        autoExpandParent={autoExpandParent}
-        onCheck={onCheck}
-        checkedKeys={checkedKeys}
-        onSelect={onSelect}
-        selectedKeys={selectedKeys}
-        treeData={treeData}
-      />
+      <Tree checkable showLine treeData={treeData} />
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const data = [
+  {
+    title: "0-0",
+    parentId: null,
+  },
+  {
+    title: "0-1",
+    parentId: null,
+  },
+  {
+    title: "0-2",
+    parentId: null,
+  },
+  {
+    title: "0-0-0",
+    parentId: "0-0",
+  },
+  {
+    title: "0-0-0-0",
+    parentId: "0-0-0",
+  },
+  {
+    title: "0-0-0-1",
+    parentId: "0-0-0",
+  },
+  {
+    title: "0-0-0-2",
+    parentId: "0-0-0",
+  },
+  {
+    title: "0-0-1",
+    parentId: "0-0",
+  },
+  {
+    title: "0-0-1-0",
+    parentId: "0-0-1",
+  },
+  {
+    title: "0-0-1-1",
+    parentId: "0-0-1",
+  },
+  {
+    title: "0-0-1-2",
+    parentId: "0-0-1",
+  },
+  {
+    title: "0-0-2",
+    parentId: "0-0",
+  },
+  {
+    title: "0-1-0",
+    parentId: "0-1",
+  },
+];
